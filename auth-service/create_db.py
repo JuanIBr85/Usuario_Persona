52ee64beafae7ebd3877c18b95e33a7e312673cf
Alvarez Eduardo <alvarez.edu26@gmail.com>
2025-05-21 09:39:47 -0300

    setup inicial:modelo usuarios, sesion y base sqllite creada

---
+++ auth-service/create_db.py
@@ -0,0 +1,6 @@
+from app.database.session import Base, engine
+from app.models.models import Usuario
+
+print(" Creando base de datos...")
+Base.metadata.create_all(bind=engine)
+print(" Base de datos creada.")
