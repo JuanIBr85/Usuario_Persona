105d47fd0ca6876a44b7aa14ca25a9bb40ef4f15
JuanIBr85 <juanignaciobauer85@gmail.com>
2025-05-21 00:15:05 -0300

    primer avance con modelos

---
+++ persona-service/backend/app/models/persona_model.py
@@ -0,0 +1,27 @@
+from extensions import db
+from sqlalchemy import ForeignKey, Integer, String, DateTime, Column, Date
+from datetime import datetime, timezone
+
+class Persona(db.Model):
+
+    __tablename__='personas'
+
+    id_persona = Column(Integer, primary_key=True)
+    nombre_persona=Column(String(50), nullable=False)
+    apellido_persona=Column(String(50),nullable=False)
+    fecha_nacimiento_persona = Column(Date,nullable=False)
+    num_doc_persona=Column(String(8), nullable=False)
+
+    #Falta completar
+    domicilio_id=Column(Integer)
+    tipo_documento_id=Column(Integer)
+    contacto_id=Column(Integer)
+
+    created_at=Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at=Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+    deleted_at=Column(DateTime, nullable=True)
+
+    # Definir relaciones con tablas "Tipo_Documento", "Contacto", Domicilio.
+
+
+
