e8e501fd0ffc6e90207e05939a17fad54261588c
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-05-24 15:54:07 -0300

    Cambios menores en el header

--- persona-service/frontend/src/components/Header.jsx
+++ persona-service/frontend/src/components/Header.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 const Header = () => {
-  return (
-    <header style={{ backgroundColor: 'var(--color-primario)' }} className="text-white p-4">
-      <h1 className="text-xl font-bold">CREUS</h1>
-    </header>
-  );
+    return (
+        <header style={{ backgroundColor: 'var(--color-primario)' }} className="text-white p-4">
+            <h1 className="flex items-center text-xl font-bold">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2"
+                >
+                    <path d="M12 7v14" />
+                    <path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" />
+                </svg>
+                CREUS
+            </h1>
+        </header>
+    );
 };
 
 export default Header;
