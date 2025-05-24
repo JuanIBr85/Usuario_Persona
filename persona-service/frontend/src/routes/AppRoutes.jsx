5bb78b6192e6f4f24f7a863969c232e7252b83cb
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-05-24 15:52:12 -0300

    agregué archivo AppRoutes.jsx en carpeta routes para definir rutas

---
+++ persona-service/frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+
+import React from 'react'
+import Profile from '../pages/Profile';
+import AdminPanel from '../pages/AdminPanel';
+import NotFound from '../pages/NotFound';
+import Login from '../pages/Login';
+import Sign from '../pages/Sign';
+import { PruebaChart } from '../pages/PruebaChart';
+function AppRoutes() {
+  return (
+    <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/adminpanel" element={<AdminPanel />} />
+        <Route path="/" element={<AdminPanel />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign" element={<Sign />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/prueba" element={<PruebaChart />} />
+      </Routes>
+  )
+}
+
+export default AppRoutes
