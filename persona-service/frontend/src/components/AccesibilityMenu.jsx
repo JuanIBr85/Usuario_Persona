41a74eeb1e3fd0d67d8184973b4e2e0a238cdd3e
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-05-25 05:22:12 -0300

    Se arreglaron errores menores

--- persona-service/frontend/src/components/AccesibilityMenu.jsx
+++ persona-service/frontend/src/components/AccesibilityMenu.jsx
@@ -18,7 +18,7 @@ const AccessibilityMenu = () => {
                 aria-expanded={showMenu}
                 aria-label="Activar menú de accesibilidad"
             >
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-person-standing-icon lucide-person-standing"><circle cx="12" cy="5" r="1" /><path d="m9 20 3-6 3 6" /><path d="m6 8 6 2 6-2" /><path d="M12 10v4" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-person-standing-icon lucide-person-standing"><circle cx="12" cy="5" r="1" /><path d="m9 20 3-6 3 6" /><path d="m6 8 6 2 6-2" /><path d="M12 10v4" /></svg>
             </button>
 
             {showMenu && (
--- persona-service/frontend/src/components/Header.jsx
+++ persona-service/frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <header className="bg-[var(--color-primario)] text-white p-4 md:px-20">
             <div className="flex flex-row justify-between items-center">
                 <div className="flex items-center" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-book-open-icon lucide-book-open"><path d="M12 7v14" /><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-book-open-icon lucide-book-open"><path d="M12 7v14" /><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" /></svg>
                     <h1 className="text-xl font-bold ml-2">CREUS</h1>
                 </div>
                 <div>
