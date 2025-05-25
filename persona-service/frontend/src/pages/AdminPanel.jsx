f84b77e7920f2634c37fe9fedaa2334e429ff1e5
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-05-25 18:31:44 -0300

    se agregó un admin panel de ejemplo

--- persona-service/frontend/src/pages/AdminPanel.jsx
+++ persona-service/frontend/src/pages/AdminPanel.jsx
@@ -1,12 +1,55 @@
 import React from 'react'
 import { Fade } from "react-awesome-reveal";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 
 function AdminPanel() {
   return (
-    <div>
+    <div className="py-5 px-20">
       <Fade duration={500} triggerOnce>
-        AdminPanel
+        <Table>
+          <TableCaption>Lista de usuarios registrados.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Nombre</TableHead>
+              <TableHead>Rol</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead className="text-right">Acciones</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">Juan Pérez</TableCell>
+              <TableCell>Profesor</TableCell>
+              <TableCell>juan.perez@email.com</TableCell>
+              <TableCell>Activo</TableCell>
+              <TableCell className="text-right space-x-2">
+                <button className="text-blue-600 hover:underline">Editar</button>
+                <button className="text-red-600 hover:underline">Borrar</button>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">Lucía Gómez</TableCell>
+              <TableCell>Alumno</TableCell>
+              <TableCell>lucia.gomez@email.com</TableCell>
+              <TableCell>Egresado</TableCell>
+              <TableCell className="text-right space-x-2">
+                <button className="text-blue-600 hover:underline">Editar</button>
+                <button className="text-red-600 hover:underline">Borrar</button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
       </Fade>
     </div>
   )
