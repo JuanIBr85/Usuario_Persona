8f2f7ec68467e4b4ce9895a50f1c30717478e89b
ifcifc <ignaciocastellanos040794@gmail.com>
2025-05-26 20:09:27 -0300

    Arreglos en algunos estilos faltantes del dark mode

--- persona-service/frontend/src/components/profile/FormServicios.jsx
+++ persona-service/frontend/src/components/profile/FormServicios.jsx
@@ -8,10 +8,10 @@ export default function FormServicios({ subscribedServices }) {
                 {subscribedServices.map((service) => (
                     <li
                         key={service}
-                        className="flex items-center justify-between bg-gray-100 rounded-lg px-4 py-2"
+                        className="flex items-center justify-between bg-gray-100 dark:bg-[var(--muted)]  rounded-lg px-4 py-2"
                     >
-                        <span className="text-gray-700">{service}</span>
-                        <Button variant="link" className="text-indigo-600 hover:text-indigo-800 p-0 h-auto">
+                        <span className="text-gray-700 dark:text-gray-100">{service}</span>
+                        <Button variant="link" className="text-indigo-600 hover:text-indigo-800 dark:text-indigo-400 p-0 h-auto">
                             Ver detalles
                         </Button>
                     </li>
