4d36ea202bd174752f456baf13b0792a72bd654a
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-05-25 23:40:38 -0300

    Se agregaron excepciones al layout

--- persona-service/frontend/src/components/Layout.jsx
+++ persona-service/frontend/src/components/Layout.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Main from "./Main";
 import AccessibilityMenu from './AccesibilityMenu';
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+  const hideHeaderFooter = ["/login", "/sign"].includes(location.pathname);
+
   return (
     <div className="flex flex-col h-screen">
-      <Header />
+      {!hideHeaderFooter && <Header />}
+      
       <div className="flex flex-1">
         <Main>{children}</Main>
       </div>
-      <Footer />
+      
+      {!hideHeaderFooter && <Footer />}
+
       <AccessibilityMenu />
     </div>
   );
--- persona-service/frontend/src/routes/AppRoutes.jsx
+++ persona-service/frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 
-import React from 'react'
+import Layout from '../components/Layout';
+
 import Profile from '../pages/Profile';
 import AdminPanel from '../pages/AdminPanel';
 import NotFound from '../pages/NotFound';
@@ -11,21 +13,27 @@ import AdminUsers from '../pages/AdminUsers';
 import AdminRoles from '../pages/AdminRoles';
 
 import { PruebaChart } from '../pages/PruebaChart';
+
 function AppRoutes() {
   return (
     <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/adminpanel" element={<AdminPanel />} />
-        <Route path="/" element={<AdminPanel />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign" element={<Sign />} />
-        <Route path="*" element={<NotFound />} />
+      {/* Rutas sin layout */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/sign" element={<Sign />} />
+
+      {/* Rutas con layout */}
+      <Route element={<Layout />}>
+        <Route index element={<AdminPanel />} /> {/* pd: esto es la raíz "/" */}
+        <Route path="profile" element={<Profile />} />
+        <Route path="adminpanel" element={<AdminPanel />} />
         <Route path="logs" element={<Logs />} />
         <Route path="adminusers" element={<AdminUsers />} />
         <Route path="adminroles" element={<AdminRoles />} />
-        <Route path="/prueba" element={<PruebaChart />} />
-      </Routes>
-  )
+        <Route path="prueba" element={<PruebaChart />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
