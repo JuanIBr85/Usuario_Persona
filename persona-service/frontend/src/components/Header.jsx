f571cd1c97155576f518de8ceed53255a05ffd3d
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-05-25 23:26:45 -0300

    Header es fixed

--- persona-service/frontend/src/components/Header.jsx
+++ persona-service/frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <header className="bg-[var(--color-primario)] text-white p-4 md:px-20">
+        <header className="fixed top-0 z-50 bg-[var(--color-primario)] text-white p-4 md:px-20 w-full">
             <div className="flex flex-row justify-between items-center">
                 <div className="flex items-center" >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-book-open-icon lucide-book-open"><path d="M12 7v14" /><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" /></svg>
