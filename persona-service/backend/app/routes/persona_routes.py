efe49b491032faf1ef675da4babde972ac90c0e1
JuanIBr85 <juanignaciobauer85@gmail.com>
2025-05-28 14:25:25 -0300

    endpoint GETs pesona y todas_personas conforme al estandar de JSON sugerido

--- persona-service/backend/app/routes/persona_routes.py
+++ persona-service/backend/app/routes/persona_routes.py
@@ -1,15 +1,75 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from app.services.persona_service import PersonaService
+from app.schema.persona_schema import PersonaSchema
 
 persona_bp = Blueprint('persona_bp', __name__)
 persona_service = PersonaService()
+persona_schema= PersonaSchema()
+
 
 # Listar personas
 @persona_bp.route('/personas', methods=['GET'])
 def listar_personas():
     personas = persona_service.listar_personas()
     return jsonify(personas), 200
+
+#Listar Todas las Personas, considerando formato JSON estandarizado por Rodrigo (Propuesta):
+@persona_bp.route('/todas_personas', methods=['GET'])
+def todas_personas():
+    try:
+        personas =persona_service.listar_personas()
+
+        if not personas:
+            return jsonify({
+                "status": "success",
+                "data": [],
+                "total": 0,
+                "message": "No se encontraron resultados"
+            }),200
+
+        return jsonify({
+            "status": "success",
+            "data": personas,
+            "total": len(personas),
+            "message": "Lista de personas obtenida"
+        }),200
+    
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": "Error al obtener personas",
+            "errors": {"server": str(e)}
+        }),500  
+    
+#Listar persona, considerando formato JSON estandarizado por Rodrigo (Propuesta):
+
+@persona_bp.route('/persona/<int:id>', methods=['GET'])
+def obtener_persona_id(id):
+
+    try:
+
+        persona = persona_service.listar_persona_id(id)
+
+        if persona is None:
+             return jsonify({
+                 "status": "error",
+                 "message": "Persona no encontrada",
+                 "errors": {"id": f"No existe persona con ID {id}"}
+                 }), 404
+        
+        return jsonify({
+            "status": "success",
+            "data": persona,
+            "message": "Persona obtenida correctamente"
+            }), 200
+    
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": "Error al obtener la persona",
+            "errors": {"server": str(e)}
+            }), 500
 
 # Buscar persona por ID
 @persona_bp.route('/personas/<int:id>', methods=['GET'])
@@ -27,6 +87,15 @@ def crear_persona():
 
         if not data:
          return jsonify({"error": "No se enviaron datos"}),400
+        
+        errors= persona_schema.validate(data)
+
+        if errors:
+            return jsonify({
+                "status": "error",
+                "message": "Error de validación",
+                "errors": errors
+            }),400
     
         persona = persona_service.crear_persona(data)
 
@@ -37,13 +106,6 @@ def crear_persona():
                 "id": persona.id_persona
             }
         }),201
-    
-    except ValidationError as err:
-        return jsonify({
-            "status": "error",
-            "message": "Error de validación",
-            "errors": err.messages
-        }),400
     
     except Exception as e:
         return jsonify({
