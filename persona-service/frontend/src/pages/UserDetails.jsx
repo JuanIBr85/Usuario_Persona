fa59879436c621b75ce8438a68c5ce46ec511277
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-05-28 18:32:29 -0300

    Se cambiaron comportamientos del header

--- persona-service/frontend/src/pages/UserDetails.jsx
+++ persona-service/frontend/src/pages/UserDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Fade } from "react-awesome-reveal";
 import { useParams } from 'react-router-dom'
 import {
     Breadcrumb,
@@ -39,68 +40,70 @@ function UserDetails() {
     }
 
     return (
-        <div className='p-6'>
-            <div className="bg-white overflow-hidden shadow rounded-lg border m-5">
-                <div className="px-4 py-5 sm:px-6">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">
-                        Perfil de Usuario
-                    </h3>
-                    <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                        Esta es información sobre el usuario.
-                    </p>
+        <div className='px-6 d-flex flex-column m-5'>
+            <Fade duration={300} triggerOnce>
+                <div className="bg-white overflow-hidden shadow rounded-lg border mb-5">
+                    <div className="px-4 py-5 sm:px-6">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">
+                            Perfil de Usuario
+                        </h3>
+                        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                            Esta es información sobre el usuario.
+                        </p>
+                    </div>
+                    <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                        <dl className="sm:divide-y sm:divide-gray-200">
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Nombre completo
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {user.nombre}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Correo electrónico
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {user.email}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Teléfono
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {user.telefono}
+                                </dd>
+                            </div>
+                            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Dirección
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2" style={{ whiteSpace: 'pre-line' }}>
+                                    {user.direccion}
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
                 </div>
-                <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-                    <dl className="sm:divide-y sm:divide-gray-200">
-                        <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt className="text-sm font-medium text-gray-500">
-                                Nombre completo
-                            </dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {user.nombre}
-                            </dd>
-                        </div>
-                        <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt className="text-sm font-medium text-gray-500">
-                                Correo electrónico
-                            </dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {user.email}
-                            </dd>
-                        </div>
-                        <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt className="text-sm font-medium text-gray-500">
-                                Teléfono
-                            </dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                {user.telefono}
-                            </dd>
-                        </div>
-                        <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt className="text-sm font-medium text-gray-500">
-                                Dirección
-                            </dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2" style={{ whiteSpace: 'pre-line' }}>
-                                {user.direccion}
-                            </dd>
-                        </div>
-                    </dl>
-                </div>
-            </div>
-            <Breadcrumb className="mt-auto self-start">
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/adminpanel">Panel De Administrador</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/adminusers">Panel De Usuarios</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>Detalles de Usuario</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
+                <Breadcrumb className="mb-4">
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/adminpanel">Panel De Administrador</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/adminusers">Panel De Usuarios</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Detalles de Usuario</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </Fade>
         </div>
     )
 }
