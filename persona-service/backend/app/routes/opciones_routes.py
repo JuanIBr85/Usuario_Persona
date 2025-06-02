2e0c30d84838a7ac44c5515d0e951af8435e19e1
JuanIBr85 <juanignaciobauer85@gmail.com>
2025-06-02 16:56:41 -0300

    se creo un metodo para generar respuestas estandares, para ser implementado en las respuestas de las rutas de los respectivos endpoints, autoria @ifcifc

--- persona-service/backend/app/routes/opciones_routes.py
+++ persona-service/backend/app/routes/opciones_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from app.utils.respuestas import respuesta_estandar,RespuestaStatus
 from app.constantes.tipos_documentos import TIPOS_DOCUMENTO_VALIDOS
 
 
@@ -6,7 +7,8 @@ opciones_bp = Blueprint("opciones_bp", __name__)
 
 @opciones_bp.route("/tipos_documento", methods=["GET"])
 def obtener_tipos_documento():
-    return jsonify({
-        "status": "success",
-        "data": TIPOS_DOCUMENTO_VALIDOS
-    }),200
+    return respuesta_estandar(
+        status=RespuestaStatus.SUCCESS,
+        message="Tipos de documento obtenidos correctamente",
+        data=TIPOS_DOCUMENTO_VALIDOS
+    ),200
--- persona-service/backend/app/services/persona_service.py
+++ persona-service/backend/app/services/persona_service.py
@@ -76,6 +76,10 @@ class PersonaService(IPersonaInterface):
 
         except Exception as e:
             session.rollback()
+
+            if "UNIQUE constraint failed: personas.usuario_id" in str(e):
+                raise Exception ("Usuario ya tiene una persona asociada, no se puede crear más de un perfil de persona por usuario")
+
             raise e
 
         finally:
@@ -107,9 +111,6 @@ class PersonaService(IPersonaInterface):
 
             if 'contacto' in data:
                 self.contacto_service.modificar_contacto(persona.contacto_id, data['contacto'], session)
-
-            #if 'tipo_documento' in data:
-             #   self.tipo_documento_service.modificar_tipo_documento(persona.tipo_documento_id, data['tipo_documento'], session)
 
             for field in ['nombre_persona', 'apellido_persona', 'fecha_nacimiento_persona', 'num_doc_persona', 'usuario_id']:
                 if field in data_validada:
