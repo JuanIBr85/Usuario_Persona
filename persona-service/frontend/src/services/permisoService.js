e3c12cc37c21cd2dcf471856141ac344acfe7748
AgusGar1 <ag89122003@gmail.com>
2025-06-10 15:33:13 -0300

    Documentacion en roleService y permisoService

--- persona-service/frontend/src/services/permisoService.js
+++ persona-service/frontend/src/services/permisoService.js
@@ -1,8 +1,13 @@
 import { fetchService, HttpMethod, ServiceURL } from "@/utils/fetchUtils";
 
+/**
+ * Servicio para la gestión de permisos
+ * Contiene métodos para obtener todos los permisos y asignarlos a roles mediante solicitudes HTTP.
+ */
 export const permisoService = {
-    showError: true,
+    showError: true, // Permite que se muestren errores si ocurre un fallo en las peticiones
 
+    // Método para obtener todos los permisos disponibles
     get_all: async () => {
         return fetchService.fetch({
             url: `${ServiceURL.auth}/super-admin/permisos`,
@@ -11,13 +16,14 @@ export const permisoService = {
         })
     },
 
+    // Método para asignar permisos a un rol específico
     asignarPermisos: async (idRol, permisos) => {
         return fetchService.fetch({
             url: `${ServiceURL.auth}/super-admin/admins/${idRol}/permisos`,
             method: HttpMethod.POST,
-            body: { permisos },
+            body: { permisos }, // Cuerpo de la solicitud: contiene la lista de permisos a asignar
             showError: permisoService.showError,
-            headers: {
+            headers: { // Encabezados HTTP que indican que el contenido enviado es JSON
                 "Content-Type": "application/json",
             },
         });
--- persona-service/frontend/src/services/roleService.js
+++ persona-service/frontend/src/services/roleService.js
@@ -1,8 +1,14 @@
 import { fetchService, HttpMethod, ServiceURL } from "@/utils/fetchUtils";
 
+/**
+ * Servicio para la gestión de roles
+ * Contiene métodos para crear, obtener y eliminar roles mediante solicitudes HTTP.
+ */
 export const roleService = {
-  showError: true,
+  showError: true, // Permite que se muestren errores si ocurre un fallo en las peticiones
 
+
+  // Metodo para crear un nuevo rol
   crear: async (body) => {
     return fetchService.fetch({
       url: `${ServiceURL.auth}/super-admin/roles`,
@@ -13,6 +19,8 @@ export const roleService = {
     })
   },
 
+
+  // Metodo para obtener la lista completa de roles
   get_all: async () => {
     return fetchService.fetch({
       url: `${ServiceURL.auth}/super-admin/roles`,
@@ -21,6 +29,8 @@ export const roleService = {
     });
   },
 
+  
+  // Método para eliminar un rol específico utilizando su ID
   borrar: async (id) => {
     return fetchService.fetch({
       url: `${ServiceURL.auth}/super-admin/roles/${id}`,
@@ -29,5 +39,4 @@ export const roleService = {
       useToken: true
     });
   },
-
 };
