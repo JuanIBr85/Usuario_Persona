d4afe9a49772c3af90c0389e793898d6a87486fa
ifcifc <ignaciocastellanos040794@gmail.com>
2025-06-18 20:40:15 -0300

    fix otp_service de persona, error de importacion

--- persona-service/backend/app/services/otp_service.py
+++ persona-service/backend/app/services/otp_service.py
@@ -1,10 +1,11 @@
-from app.utils.email_utils import generar_codigo_otp, enviar_codigo_por_email_persona
+from app.utils.email_util import generar_codigo_otp, enviar_codigo_por_email_persona
+
 
 class OtpService:
-    #genera y envia codigo otp
+    # genera y envia codigo otp
 
     def solicitar_otp(self, persona) -> str:
-    
+
         codigo = generar_codigo_otp()
         enviar_codigo_por_email_persona(persona, codigo)
         return codigo
