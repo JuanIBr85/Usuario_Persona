dcc5b733704a0d8a093aa2bb69fc8777620ffa7e
ifcifc <ignaciocastellanos040794@gmail.com>
2025-06-19 01:30:46 -0300

    ahora usa la api gateway para obtener los datos del cliente

--- auth-service/app/routes/usuarios_blueprint.py
+++ auth-service/app/routes/usuarios_blueprint.py
@@ -18,92 +18,142 @@ from common.models.cache_settings import CacheSettings
 usuario_bp = Blueprint("usuario", __name__)
 usuario_service = UsuarioService()
 
-#-----------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------
 # REGISTRO Y VERIFICACIÓN
-#-----------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------
 
-@usuario_bp.route('/registro', methods=['POST'])
+
+@usuario_bp.route("/registro", methods=["POST"])
 @api_access(is_public=True, limiter=["2 per minute"])
 def registrar_usuario1():
     session = SessionLocal()
     try:
         data = request.get_json()
         if not data:
-            return make_response(ResponseStatus.FAIL, "Datos de entrada requeridos", error_code="NO_INPUT")
+            return make_response(
+                ResponseStatus.FAIL,
+                "Datos de entrada requeridos",
+                error_code="NO_INPUT",
+            )
 
         status, mensaje, data, code = usuario_service.registrar_usuario(session, data)
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        print(" ERROR INTERNO EN /registro:", str(e)) 
-        return make_response(ResponseStatus.ERROR, "Error al registrar usuario", str(e), error_code="REGISTRO_ERROR"), 500
+        print(" ERROR INTERNO EN /registro:", str(e))
+        return (
+            make_response(
+                ResponseStatus.ERROR,
+                "Error al registrar usuario",
+                str(e),
+                error_code="REGISTRO_ERROR",
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-@usuario_bp.route('/verificar-email', methods=['GET'])
+@usuario_bp.route("/verificar-email", methods=["GET"])
 @api_access(is_public=True, limiter=["1 per minute"])
 def verificar_email():
     session = SessionLocal()
     try:
-        token = request.args.get('token')
+        token = request.args.get("token")
         if not token:
-            return make_response(ResponseStatus.FAIL, "Token de verificación requerido", error_code="TOKEN_MISSING"), 400
+            return (
+                make_response(
+                    ResponseStatus.FAIL,
+                    "Token de verificación requerido",
+                    error_code="TOKEN_MISSING",
+                ),
+                400,
+            )
 
         status, mensaje, data, code = usuario_service.verificar_email(session, token)
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        return make_response(ResponseStatus.ERROR, "Error al verificar email", str(e), error_code="VERIFICACION_ERROR"), 500
+        return (
+            make_response(
+                ResponseStatus.ERROR,
+                "Error al verificar email",
+                str(e),
+                error_code="VERIFICACION_ERROR",
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-#-----------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------
 # LOGIN Y LOGOUT
-#-----------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------
 
-@usuario_bp.route('/login', methods=['POST'])
+
+@usuario_bp.route("/login", methods=["POST"])
 @api_access(is_public=True, limiter=["3 per minute"])
 def login1():
     session = SessionLocal()
     try:
         data = request.get_json()
         if not data:
-            return make_response(ResponseStatus.FAIL, "Datos de entrada requeridos", error_code="NO_INPUT")
-        
-        user_agent = request.headers.get('User-Agent', '')
-        ip = request.remote_addr
+            return make_response(
+                ResponseStatus.FAIL,
+                "Datos de entrada requeridos",
+                error_code="NO_INPUT",
+            )
 
-        status, mensaje, data, code = usuario_service.login_usuario(session, data,user_agent,ip)
+        user_agent = request.headers.get("X-CLIENT-USER-AGENT", "")
+        ip = request.headers.get("X-CLIENT-IP", "")
+
+        status, mensaje, data, code = usuario_service.login_usuario(
+            session, data, user_agent, ip
+        )
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        return make_response(ResponseStatus.ERROR, "Error en login", str(e), error_code="LOGIN_ERROR"), 500
+        return (
+            make_response(
+                ResponseStatus.ERROR, "Error en login", str(e), error_code="LOGIN_ERROR"
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-@usuario_bp.route('/logout', methods=['POST'])
+@usuario_bp.route("/logout", methods=["POST"])
 @api_access(is_public=True)
 def logout_usuario():
     session = SessionLocal()
     try:
         usuario_id = request.jwt_payload["sub"]
-        status, mensaje, data, code = usuario_service.logout_usuario(session, usuario_id)
+        status, mensaje, data, code = usuario_service.logout_usuario(
+            session, usuario_id
+        )
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        return make_response(ResponseStatus.ERROR, "Error al hacer logout", str(e), error_code="LOGOUT_ERROR"), 500
+        return (
+            make_response(
+                ResponseStatus.ERROR,
+                "Error al hacer logout",
+                str(e),
+                error_code="LOGOUT_ERROR",
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-@usuario_bp.route('/perfil', methods=['GET'])
+@usuario_bp.route("/perfil", methods=["GET"])
 @api_access(is_public=True, limiter=["4 per minute"])
 def perfil_usuario():
     session = SessionLocal()
@@ -113,58 +163,101 @@ def perfil_usuario():
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        return make_response(ResponseStatus.ERROR, "Error al obtener perfil", str(e), error_code="PERFIL_ERROR"), 500
+        return (
+            make_response(
+                ResponseStatus.ERROR,
+                "Error al obtener perfil",
+                str(e),
+                error_code="PERFIL_ERROR",
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-#-----------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------
 # RECUPERACIÓN DE PASSWORD CON OTP
-#-----------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------
 
-@usuario_bp.route('/solicitar-otp', methods=['POST'])
+
+@usuario_bp.route("/solicitar-otp", methods=["POST"])
 @api_access(is_public=True, limiter=["1 per 5 minutes"])
 def solicitar_otp():
     session = SessionLocal()
     try:
-        email = request.json.get('email')
+        email = request.json.get("email")
         if not email:
-            return make_response(ResponseStatus.FAIL, "Email requerido", error_code="EMAIL_REQUIRED"), 400
+            return (
+                make_response(
+                    ResponseStatus.FAIL, "Email requerido", error_code="EMAIL_REQUIRED"
+                ),
+                400,
+            )
 
-        status, mensaje, data, code = usuario_service.solicitar_codigo_reset(session, email)
+        status, mensaje, data, code = usuario_service.solicitar_codigo_reset(
+            session, email
+        )
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        return make_response(ResponseStatus.ERROR, "Error al solicitar OTP", str(e), error_code="OTP_SOLICITUD_ERROR"), 500
+        return (
+            make_response(
+                ResponseStatus.ERROR,
+                "Error al solicitar OTP",
+                str(e),
+                error_code="OTP_SOLICITUD_ERROR",
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-@usuario_bp.route('/verificar-otp', methods=['POST'])
-@api_access(is_public=True, limiter=["3 per minute"], cache=CacheSettings(expiration=60, params=["email", "otp"]))
+@usuario_bp.route("/verificar-otp", methods=["POST"])
+@api_access(
+    is_public=True,
+    limiter=["3 per minute"],
+    cache=CacheSettings(expiration=60, params=["email", "otp"]),
+)
 def verificar_otp():
     session = SessionLocal()
     try:
         data = request.get_json()
-        email = data.get('email')
-        otp = data.get('otp')
+        email = data.get("email")
+        otp = data.get("otp")
 
         if not email or not otp:
-            return make_response(ResponseStatus.FAIL, "Email y OTP son requeridos", error_code="FALTAN_DATOS"), 400
+            return (
+                make_response(
+                    ResponseStatus.FAIL,
+                    "Email y OTP son requeridos",
+                    error_code="FALTAN_DATOS",
+                ),
+                400,
+            )
 
         status, mensaje, data, code = usuario_service.verificar_otp(session, email, otp)
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        return make_response(ResponseStatus.ERROR, "Error al verificar OTP", str(e), error_code="OTP_VERIFICACION_ERROR"), 500
+        return (
+            make_response(
+                ResponseStatus.ERROR,
+                "Error al verificar OTP",
+                str(e),
+                error_code="OTP_VERIFICACION_ERROR",
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-@usuario_bp.route('/reset-password-con-codigo', methods=['POST'])
+@usuario_bp.route("/reset-password-con-codigo", methods=["POST"])
 @api_access(is_public=True, limiter=["2 per minute"])
 def reset_con_otp():
     session = SessionLocal()
@@ -172,35 +265,57 @@ def reset_con_otp():
         data = request.get_json()
         token = request.headers.get("Authorization", "").replace("Bearer ", "")
         if not data or not token:
-            return make_response(ResponseStatus.FAIL, "Datos requeridos y token requeridos", error_code="NO_INPUT"), 400
-        
+            return (
+                make_response(
+                    ResponseStatus.FAIL,
+                    "Datos requeridos y token requeridos",
+                    error_code="NO_INPUT",
+                ),
+                400,
+            )
+
         email_from_token = verificar_token_reset(token)
         if not email_from_token:
             return make_response(
                 ResponseStatus.UNAUTHORIZED,
                 "Token inválido o expirado",
-                error_code="TOKEN_INVALID"
+                error_code="TOKEN_INVALID",
             )
 
-        status, mensaje, data, code = usuario_service.cambiar_password_con_codigo(session, data, token, email_from_token)
+        status, mensaje, data, code = usuario_service.cambiar_password_con_codigo(
+            session, data, token, email_from_token
+        )
         return make_response(status, mensaje, data, code), code
 
     except Exception as e:
-        return make_response(ResponseStatus.ERROR, "Error al cambiar contraseña", str(e), error_code="RESET_ERROR"), 500
+        return (
+            make_response(
+                ResponseStatus.ERROR,
+                "Error al cambiar contraseña",
+                str(e),
+                error_code="RESET_ERROR",
+            ),
+            500,
+        )
 
     finally:
         session.close()
 
 
-@usuario_bp.route('/modificar', methods=['GET', 'POST'])
+@usuario_bp.route("/modificar", methods=["GET", "POST"])
 @api_access(is_public=True, limiter=["2 per minute"])
 def modificar_perfil():
     return make_response(ResponseStatus.PENDING, "Funcionalidad en construcción"), 501
 
-@usuario_bp.route('/verificar-dispositivo', methods=['GET'])
-@api_access(is_public=True, limiter=["2 per minute"], cache=CacheSettings(expiration=30, params=["token"]))
+
+@usuario_bp.route("/verificar-dispositivo", methods=["GET"])
+@api_access(
+    is_public=True,
+    limiter=["2 per minute"],
+    cache=CacheSettings(expiration=30, params=["token"]),
+)
 def verificar_dispositivo():
-    token = request.args.get('token')
+    token = request.args.get("token")
     if not token:
         return "Token faltante", 400
 
@@ -226,17 +341,20 @@ def verificar_dispositivo():
     nuevo_dispositivo = DispositivoConfiable(
         usuario_id=usuario.id_usuario,
         user_agent=user_agent,
-        token_dispositivo= generar_token_dispositivo(email, user_agent, ip),
-        fecha_registro = datetime.now(timezone.utc),
-        fecha_expira = datetime.now(timezone.utc) + timedelta(days=30)
+        token_dispositivo=generar_token_dispositivo(email, user_agent, ip),
+        fecha_registro=datetime.now(timezone.utc),
+        fecha_expira=datetime.now(timezone.utc) + timedelta(days=30),
     )
     session.add(nuevo_dispositivo)
     session.commit()
 
     return "Dispositivo confirmado. Ahora podés volver a iniciar sesión.", 200
 
-@usuario_bp.route('/refresh', methods=['POST'])
-@api_access(is_public=True)  # solo controla si es pública, pero no te ayuda con el refresh_token
+
+@usuario_bp.route("/refresh", methods=["POST"])
+@api_access(
+    is_public=True
+)  # solo controla si es pública, pero no te ayuda con el refresh_token
 def refresh_token():
     session = SessionLocal()
     try:
@@ -247,29 +365,31 @@ def refresh_token():
             return Response(
                 json.dumps({"error": "Token de refresh requerido"}),
                 status=400,
-                mimetype='application/json'
+                mimetype="application/json",
             )
 
         # Validar y decodificar manualmente el refresh token
         try:
-            payload = jwt.decode(token, getenv("JWT_SECRET_KEY", "clave_jwt_123"), algorithms=["HS256"])
+            payload = jwt.decode(
+                token, getenv("JWT_SECRET_KEY", "clave_jwt_123"), algorithms=["HS256"]
+            )
             if payload.get("scope") != "refresh_token":
                 return Response(
                     json.dumps({"error": "Token inválido para refresh"}),
                     status=401,
-                    mimetype='application/json'
+                    mimetype="application/json",
                 )
         except jwt.ExpiredSignatureError:
             return Response(
                 json.dumps({"error": "Token de refresh expirado"}),
                 status=401,
-                mimetype='application/json'
+                mimetype="application/json",
             )
         except jwt.InvalidTokenError:
             return Response(
                 json.dumps({"error": "Token inválido"}),
                 status=401,
-                mimetype='application/json'
+                mimetype="application/json",
             )
 
         # Usar el sub (id de usuario) para generar un nuevo access token
@@ -279,10 +399,8 @@ def refresh_token():
         return Response(
             json.dumps(result),
             status=result.get("code", 200),
-            mimetype='application/json'
+            mimetype="application/json",
         )
 
     finally:
         session.close()
-
-
