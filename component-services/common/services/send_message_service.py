f6b6cd7f78ef9ea2bd4a6033d310f1d64c7f81cd
ifcifc <ignaciocastellanos040794@gmail.com>
2025-06-21 16:09:33 -0300

    Fix varios

--- component-services/common/services/send_message_service.py
+++ component-services/common/services/send_message_service.py
@@ -3,12 +3,14 @@ import uuid
 import requests
 import traceback
 from common.schemas.message_schema import MessageSchema
-
+import os
+import logging
 message_schema = MessageSchema()
-
-
+logger = logging.getLogger(__name__)
+COMPONENT_SERVICE_HOST = os.environ.get("COMPONENT_SERVICE_HOST", "localhost")  # Default to auth-service if not set
+logger.error(COMPONENT_SERVICE_HOST)
 def send_message(
-    to_service: str, channel: str = "default", event_type: str = "", message: dict = {}
+    to_service: str, channel: str = "default", event_type: str = "default", message: dict = {}
 ):
     try:
         # Obtenemos el nombre del servicio desde el header
@@ -28,9 +30,15 @@ def send_message(
         if error:
             return None, 400, error
 
+
+        
+        logger.error(
+            f"Enviando mensaje a {to_service} en el canal {channel}: {message}"
+        )
+
         # Enviamos el mensaje
         response = requests.post(
-            f"http://localhost:5002/internal/send",
+            f"http://{COMPONENT_SERVICE_HOST}:5002/internal/send",
             json=message,
             timeout=2,
         )
@@ -41,7 +49,7 @@ def send_message(
                 response.text or "Error al enviar mensaje",
             )
 
-        return response.text, response.status_code, None
+        return message["message_id"], response.status_code, None
     except Exception as e:
         traceback.print_exc()
         return None, 500, str(e)
