477dfdaea03e293a16716052365c4c7266428aaa
ifcifc <ignaciocastellanos040794@gmail.com>
2025-06-26 15:08:58 -0300

    Fix para DB de auth para esta rama

--- auth-service/app/database/session.py
+++ auth-service/app/database/session.py
@@ -4,19 +4,23 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from app.models.base_model import Base
 from dotenv import load_dotenv
 
-load_dotenv()  
 
-DB_USER = os.getenv("POSTGRES_USER")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_NAME = os.getenv("POSTGRES_DB")
-DB_HOST = os.getenv("POSTGRES_HOST")
-DB_PORT = os.getenv("POSTGRES_PORT")
+load_dotenv()
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DB_USER = os.getenv("POSTGRES_USER")
+# DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+# DB_NAME = os.getenv("POSTGRES_DB")
+# DB_HOST = os.getenv("POSTGRES_HOST")
+# DB_PORT = os.getenv("POSTGRES_PORT")
+
+DATABASE_URL = "sqlite:///auth.db"
 
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+SessionLocal = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
+
 
 # Exponer DB_PATH para otros scripts (como reset_db.py)
 __all__ = ["Base", "engine", "SessionLocal"]
--- auth-service/run.py
+++ auth-service/run.py
@@ -1,7 +1,14 @@
 from app import create_app
 import os
+from app.script.reset_db import eliminar_base, crear_base
+from app.script.seed_data import seed
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=os.getenv("FLASK_DEBUG", False), host='0.0.0.0', port=5000)
+# Fix para recrear la DB
+eliminar_base()
+crear_base()
+seed()
+
+if __name__ == "__main__":
+    app.run(debug=os.getenv("FLASK_DEBUG", False), host="0.0.0.0", port=5000)
