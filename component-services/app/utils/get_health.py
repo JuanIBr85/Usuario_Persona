154f46c15890dcf1ea9c79ff5423cc6005d17a7f
ifcifc <ignaciocastellanos040794@gmail.com>
2025-06-26 14:43:05 -0300

    Fix & Cache

--- component-services/app/utils/get_health.py
+++ component-services/app/utils/get_health.py
@@ -1,11 +1,19 @@
 import time
 import requests
+from cachetools import TTLCache
+
+
+health_cache = TTLCache(maxsize=100, ttl=30)
 
 
 # Obtiene la informacion desde los servicios
 def get_health(service_url) -> bool:
     try:
-        req = requests.get(f"{service_url}/component_service/health")
-        return req.status_code == 200
+        if service_url in health_cache:
+            return health_cache[service_url]
+        req = requests.get(f"{service_url}/component_service/health", timeout=2)
+        status = req.status_code == 200
+        health_cache[service_url] = status
+        return status
     except:
         return False
