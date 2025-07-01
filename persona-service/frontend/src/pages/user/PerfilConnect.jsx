d85621e3fbeaae27834184ef7a9bf4546157f1a0
Acosta-gh <79601880+Acosta-gh@users.noreply.github.com>
2025-07-01 16:49:34 -0300

    Se agregó validatePattern y validationMessage a PerfilConnect.jsx

--- persona-service/frontend/src/pages/user/PerfilConnect.jsx
+++ persona-service/frontend/src/pages/user/PerfilConnect.jsx
@@ -50,7 +50,7 @@ function PerfilConnect() {
             .catch(error => {
                 console.log(error)
 
-                if(error.statusCode === 404){
+                if (error.statusCode === 404) {
                     alert("No se encontro el documento, vamos a crear un nuevo perfil");
                     navigate("/createperfil")
                 }
@@ -67,16 +67,16 @@ function PerfilConnect() {
             ...tempData,
             ...formData
         })
-        .then(response => {
-            setTempData({...response.data});
-            api?.scrollNext()
-        })
-        .catch(error => {
-            console.error(error)
-        })
-        .finally(() => {
-            setLoading(false)
-        })
+            .then(response => {
+                setTempData({ ...response.data });
+                api?.scrollNext()
+            })
+            .catch(error => {
+                console.error(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
     }
 
     const handleOTPVerification = async (event) => {
@@ -86,15 +86,15 @@ function PerfilConnect() {
             ...tempData,
             ...formData
         })
-        .then(response => {
-            navigate('/profile')
-        })
-        .catch(error => {
-            console.error(error)
-        })
-        .finally(() => {
-            setLoading(false)
-        })
+            .then(response => {
+                navigate('/profile')
+            })
+            .catch(error => {
+                console.error(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
     }
 
     return (
@@ -109,50 +109,50 @@ function PerfilConnect() {
             <Fade duration={300} triggerOnce>
                 <div className="w-full flex items-center justify-center sm:p-4">
                     <Card className="w-full max-w-2xl shadow-lg rounded-xl overflow-hidden">
-                            <CardHeader className="text-center">
-                                <CardTitle className="text-2xl">
-                                    Vinculacion de perfil
-                                </CardTitle>
-                                <CardDescription>
-                                    <p>Conecta tu perfil con tu cuenta de usuario</p>
-                                    <p>Verificaremos si estas en el sistema</p>
-                                </CardDescription>
-                            </CardHeader>
+                        <CardHeader className="text-center">
+                            <CardTitle className="text-2xl">
+                                Vinculacion de perfil
+                            </CardTitle>
+                            <CardDescription>
+                                <p>Conecta tu perfil con tu cuenta de usuario</p>
+                                <p>Verificaremos si estas en el sistema</p>
+                            </CardDescription>
+                        </CardHeader>
 
 
-                            <SimpleCarousel setApi={setApi}>
-                                <CardContent className="h-full overflow-y-auto ">
-                                    <form onSubmit={handleDNIVerificacion} className="flex flex-col gap-8">
-                                        <SimpleSelect
-                                            name="tipo_documento"
-                                            label="Tipo de documento"
-                                            placeholder="Selecciona un tipo de documento"
-                                            required
-                                        >
-                                            {tipoDocumento.map((tipo) => (
+                        <SimpleCarousel setApi={setApi}>
+                            <CardContent className="h-full overflow-y-auto ">
+                                <form onSubmit={handleDNIVerificacion} className="flex flex-col gap-8">
+                                    <SimpleSelect
+                                        name="tipo_documento"
+                                        label="Tipo de documento"
+                                        placeholder="Selecciona un tipo de documento"
+                                        required
+                                    >
+                                        {tipoDocumento.map((tipo) => (
                                             <SelectItem key={tipo} value={tipo}>
                                                 {tipo}
                                             </SelectItem>
-                                            ))}
-                                        </SimpleSelect>
-                                        <InputValidate
-                                            id="num_doc_persona"
-                                            type="number"
-                                            labelText="Ingresa el número de documento"
-                                            placeholder="Nº de documento"
-                                            containerClassName="sm:col-span-3"
-                                            required
-                                        />
-                                        <Button type="submit" className="w-full">
-                                            Siguiente
-                                        </Button>
-                                    </form>
-                                </CardContent>
+                                        ))}
+                                    </SimpleSelect>
+                                    <InputValidate
+                                        id="num_doc_persona"
+                                        type="number"
+                                        labelText="Ingresa el número de documento"
+                                        placeholder="Nº de documento"
+                                        containerClassName="sm:col-span-3"
+                                        required
+                                    />
+                                    <Button type="submit" className="w-full">
+                                        Siguiente
+                                    </Button>
+                                </form>
+                            </CardContent>
 
-                                <CardContent className="h-full overflow-y-auto">
+                            <CardContent className="h-full overflow-y-auto">
                                 <form onSubmit={handleEmailVerification} className="flex flex-col gap-4">
                                     <InputValidate
-                                        type="text"
+                                        type="email"
                                         labelText="¿Es este tu email?"
                                         value={email}
                                         containerClassName="sm:col-span-3"
@@ -160,10 +160,12 @@ function PerfilConnect() {
                                     />
                                     <InputValidate
                                         id="email_confirmado"
-                                        type="text"
+                                        type="email"
                                         labelText="Escribe tu email"
                                         placeholder={email}
                                         containerClassName="sm:col-span-3"
+                                        validatePattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+                                        validationMessage="Por favor, ingresa un correo electrónico válido."
                                     />
                                     <Button type="submit" className="w-full">
                                         Siguiente
@@ -171,84 +173,84 @@ function PerfilConnect() {
                                     <Button className="w-full" onClick={() => api?.scrollTo(3)}>
                                         No es mi correo / Ya no uso ese correo
                                     </Button>
-                                    </form>
-                                </CardContent>
+                                </form>
+                            </CardContent>
 
-                                <CardContent className="h-full overflow-y-auto flex flex-col gap-8">
-                                    <form onSubmit={handleOTPVerification} className="grid w-full items-center justify-center gap-4">
-                                        <Label htmlFor="codigo" className="inline-block w-full text-center">Código de verificación</Label>
-                                        <div className="relative">
-                                            <InputOTP name="codigo" maxLength={6} containerClassName="justify-center">
-                                                <InputOTPGroup>
-                                                    <InputOTPSlot className="bg-gray-100" index={0} />
-                                                    <InputOTPSlot className="bg-gray-100" index={1} />
-                                                    <InputOTPSlot className="bg-gray-100" index={2} />
-                                                </InputOTPGroup>
-                                                <InputOTPSeparator />
-                                                <InputOTPGroup>
-                                                    <InputOTPSlot className="bg-gray-100" index={3} />
-                                                    <InputOTPSlot className="bg-gray-100" index={4} />
-                                                    <InputOTPSlot className="bg-gray-100" index={5} />
-                                                </InputOTPGroup>
-                                            </InputOTP>
-                                        </div>
-                                        <Button type="submit" className="w-full">
-                                            Siguiente
-                                        </Button>
-                                    </form>
-                                </CardContent>
-
-                                <CardContent className="h-full overflow-y-auto flex flex-col gap-4">
-                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-                                        <InputValidate
-                                            id="nombre_persona"
-                                            type="text"
-                                            labelText="Nombre"
-                                        />
-                                        <InputValidate
-                                            id="apellido_persona"
-                                            type="text"
-                                            labelText="Apellido"
-                                        />
+                            <CardContent className="h-full overflow-y-auto flex flex-col gap-8">
+                                <form onSubmit={handleOTPVerification} className="grid w-full items-center justify-center gap-4">
+                                    <Label htmlFor="codigo" className="inline-block w-full text-center">Código de verificación</Label>
+                                    <div className="relative">
+                                        <InputOTP name="codigo" maxLength={6} containerClassName="justify-center">
+                                            <InputOTPGroup>
+                                                <InputOTPSlot className="bg-gray-100" index={0} />
+                                                <InputOTPSlot className="bg-gray-100" index={1} />
+                                                <InputOTPSlot className="bg-gray-100" index={2} />
+                                            </InputOTPGroup>
+                                            <InputOTPSeparator />
+                                            <InputOTPGroup>
+                                                <InputOTPSlot className="bg-gray-100" index={3} />
+                                                <InputOTPSlot className="bg-gray-100" index={4} />
+                                                <InputOTPSlot className="bg-gray-100" index={5} />
+                                            </InputOTPGroup>
+                                        </InputOTP>
                                     </div>
-                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-                                        <InputValidate
-                                            id="fecha_nacimiento_persona"
-                                            type="date"
-                                            placeholder="Ingresa tu fecha de nacimiento"
-                                            labelText="Fecha de nacimiento"
-                                            validationMessage="La fecha de nacimiento es requerida"
-                                            required
-                                        />
-                                        <InputValidate
-                                            id="telefono_movil"
-                                            name="telefono_movil"
-                                            type="tel"
-                                            placeholder="Ingresa tu teléfono móvil"
-                                            labelText="Teléfono móvil"
-                                            validatePattern="^[\+]?[0-9\-\s\(\)]{10,}$"
-                                            validationMessage="Ingresa un número de teléfono válido"
-                                            required
-                                        />
-                                    </div>
-                                    <Button type="submit" className="w-full" onClick={() => setDialog({
-                                        title: "Verificar identidad",
-                                        description: <>
-                                            Tus datos concuerdan, enviaremos una peticion de verificacion al administrador, te contactaremos pronto
-                                            <br />
-                                            En caso de no ser contactado, puedes contactarnos al correo <a className="text-blue-500" href="mailto:soporte@persona.com">soporte@persona.com</a>
-                                            <br />
-                                            o llamar al numero <a className="text-blue-500" href="tel:+56912345678">+56912345678</a>
-                                        </>
-                                    })}>
-                                        Verificar identidad
+                                    <Button type="submit" className="w-full">
+                                        Siguiente
                                     </Button>
-                                </CardContent>
-                            </SimpleCarousel>
+                                </form>
+                            </CardContent>
 
-                            <CardFooter className="flex justify-between text-sm text-gray-500 border-t">
-                            </CardFooter>
-                        </Card>
+                            <CardContent className="h-full overflow-y-auto flex flex-col gap-4">
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+                                    <InputValidate
+                                        id="nombre_persona"
+                                        type="text"
+                                        labelText="Nombre"
+                                    />
+                                    <InputValidate
+                                        id="apellido_persona"
+                                        type="text"
+                                        labelText="Apellido"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+                                    <InputValidate
+                                        id="fecha_nacimiento_persona"
+                                        type="date"
+                                        placeholder="Ingresa tu fecha de nacimiento"
+                                        labelText="Fecha de nacimiento"
+                                        validationMessage="La fecha de nacimiento es requerida"
+                                        required
+                                    />
+                                    <InputValidate
+                                        id="telefono_movil"
+                                        name="telefono_movil"
+                                        type="tel"
+                                        placeholder="Ingresa tu teléfono móvil"
+                                        labelText="Teléfono móvil"
+                                        validatePattern="^[\+]?[0-9\-\s\(\)]{10,}$"
+                                        validationMessage="Ingresa un número de teléfono válido"
+                                        required
+                                    />
+                                </div>
+                                <Button type="submit" className="w-full" onClick={() => setDialog({
+                                    title: "Verificar identidad",
+                                    description: <>
+                                        Tus datos concuerdan, enviaremos una peticion de verificacion al administrador, te contactaremos pronto
+                                        <br />
+                                        En caso de no ser contactado, puedes contactarnos al correo <a className="text-blue-500" href="mailto:soporte@persona.com">soporte@persona.com</a>
+                                        <br />
+                                        o llamar al numero <a className="text-blue-500" href="tel:+56912345678">+56912345678</a>
+                                    </>
+                                })}>
+                                    Verificar identidad
+                                </Button>
+                            </CardContent>
+                        </SimpleCarousel>
+
+                        <CardFooter className="flex justify-between text-sm text-gray-500 border-t">
+                        </CardFooter>
+                    </Card>
                 </div>
             </Fade>
         </>
