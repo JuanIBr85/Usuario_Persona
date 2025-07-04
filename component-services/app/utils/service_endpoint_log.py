dfee939e14b6d024c66249d48fcb2ba831014da7
ifcifc <ignaciocastellanos040794@gmail.com>
2025-07-04 13:52:11 -0300

    Documentacion

--- component-services/app/utils/service_endpoint_log.py
+++ component-services/app/utils/service_endpoint_log.py
@@ -1,8 +1,13 @@
 import time
-
 from enum import Enum
 
+"""
+Clase que representa el log de un servicio
+La funcion de esto es hacer un log mas limpio y sin tantos string 
+"""
+
 class EnumSuccess(str, Enum):
+    """Enum para settear el estado del log"""
     IN_PROGRESS = "in_progress"
     SUCCESS = "success"
     ERROR = "error"
@@ -13,13 +18,13 @@ class EnumSuccess(str, Enum):
 
 class ServiceEndpointLog:
     """Clase que representa el log de un servicio"""
-    _progress:bool
-    success:EnumSuccess
-    start_time:float
-    end_time:float|None
-    error:str|None
-    endpoints_count:int
-    service_name:str
+    _progress:bool # Indica si la busqueda de endpoints esta en progreso
+    success:EnumSuccess # Indica el estado de la busqueda de endpoints
+    start_time:float # Tiempo de inicio
+    end_time:float|None # Tiempo de finalización
+    error:str|None # Error
+    endpoints_count:int # Cantidad de endpoints
+    service_name:str # Nombre del servicio
 
     def __init__(self, service_name:str):
         self.service_name = service_name
@@ -31,30 +36,35 @@ class ServiceEndpointLog:
         self.endpoints_count = 0
     
     def set_state(self, state:EnumSuccess):
-        """Setea el estado del servicio"""
+        """Setea el estado de la busqueda de endpoints"""
         self.success = state
         self._progress = False
         self.end_time = time.time()
 
     def set_success(self, endpoints_count:int):
+        """Setea el estado de la busqueda de endpoints como exitoso"""
         self.set_state(EnumSuccess.SUCCESS)
         self.endpoints_count = endpoints_count  
     
     def set_error(self, error:str):
+        """Setea el estado de la busqueda de endpoints como error"""
         self.set_state(EnumSuccess.ERROR)
         self.error = error
     
     def set_not_available(self):
+        """Setea el estado de la busqueda de endpoints como no disponible"""
         self.set_state(EnumSuccess.NOT_AVAILABLE)
     
     def set_stop(self):
+        """Setea el estado de la busqueda de endpoints como detenido"""
         self.set_state(EnumSuccess.STOP)
     
     def set_timeout(self):
+        """Setea el estado de la busqueda de endpoints como timeout"""
         self.set_state(EnumSuccess.TIMEOUT)
     
-
     def to_dict(self):
+        """Devuelve el log como un diccionario"""
         return {
             "progress": self._progress,
             "success": self.success,
