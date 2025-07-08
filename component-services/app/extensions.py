658873a8bc036c35d2c6b3b0b614d328c2720b77
ifcifc <ignaciocastellanos040794@gmail.com>
2025-07-08 01:10:57 -0300

    Se creo un archivo separado para controlar las CORS. Ahora los endpoints de component pueden manejar cache. Se eliminaron rutas sin uso. Ahora los permisos son comprobados de forma mas estricta. Se actualizaron las variables de entorno del .env.example. Se creo un nuevo decorador para el cache. Se actualizo common.

--- component-services/app/extensions.py
+++ component-services/app/extensions.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 from flask_jwt_extended import JWTManager
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from config import SQLALCHEMY_DATABASE_URI, SERVICES_CONFIG_FILE
+from config import SQLALCHEMY_DATABASE_URI, SERVICES_CONFIG_FILE, REDIS_HOST, REDIS_PORT
 #from diskcache import FanoutCache
 import logging
 import redis
@@ -28,7 +28,7 @@ services_config = []  # json.load(open(SERVICES_CONFIG_FILE))
 limiter = Limiter(
     key_func=get_remote_address, 
     default_limits=["100/minute"],
-    enabled=False#Para desactivar durante los berchmarks
+    enabled=True#Para desactivar durante los berchmarks
 )
 
 # Este es el cache de los endpoints
@@ -47,8 +47,8 @@ redis_client_core = None # Es para intercambiar mensajes para sincronizar los wo
 redis_client_cache = None # Este es para manejar cualquier dato de cache
 try:
     redis_client_auth = redis.StrictRedis(
-        host=os.getenv("REDIS_HOST", "localhost"),
-        port=int(os.getenv("REDIS_PORT", 6379)),
+        host=REDIS_HOST,
+        port=REDIS_PORT,
         db=0,
         decode_responses=True,
     )
@@ -56,21 +56,22 @@ try:
     print("[✓] Redis auth conectado correctamente.")
 
     redis_client_core = redis.StrictRedis(
-        host=os.getenv("REDIS_HOST", "localhost"),
-        port=int(os.getenv("REDIS_PORT", 6379)),
+        host=REDIS_HOST,
+        port=REDIS_PORT,
         db=1,
         decode_responses=True,
     )
     redis_client_core.ping()  # test de conexión
+    print("[✓] Redis core conectado correctamente.")
 
     redis_client_cache = redis.StrictRedis(
-        host=os.getenv("REDIS_HOST", "localhost"),
-        port=int(os.getenv("REDIS_PORT", 6379)),
+        host=REDIS_HOST,
+        port=REDIS_PORT,
         db=2,
         decode_responses=False,
     )
-    redis_client_core.ping()  # test de conexión
-    print("[✓] Redis core conectado correctamente.")
+    redis_client_cache.ping()  # test de conexión
+    print("[✓] Redis cache conectado correctamente.")
 except Exception as e:
     print("[x] Error al conectar con Redis:", e)
     raise e
