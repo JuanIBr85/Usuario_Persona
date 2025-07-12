fd892e0bf153cb330de2b036b8893899f81ed78b
ifcifc <ignaciocastellanos040794@gmail.com>
2025-07-11 22:56:18 -0300

    Forma de activar y desactivar los console logs

--- persona-service/frontend/src/App.jsx
+++ persona-service/frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import Layout from './components/Layout';
 import AppRoutes from "./routes/AppRoutes";
 
+if(import.meta.env.VITE_DEBUG==="0"){
+  console.log = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
 function App() {
   return (
       <Layout>
