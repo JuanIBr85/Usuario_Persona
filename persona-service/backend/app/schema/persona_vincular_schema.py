eaf08adb36159cb25a93bda99810ff01a1bb1516
ifcifc <ignaciocastellanos040794@gmail.com>
2025-07-12 11:40:39 -0300

    Formateo de CUIL/CUIT

--- persona-service/backend/app/schema/persona_vincular_schema.py
+++ persona-service/backend/app/schema/persona_vincular_schema.py
@@ -1,13 +1,14 @@
 from marshmallow import Schema, fields, validate
 from config import TIPOS_DOCUMENTO_VALIDOS
+from app.schema.persona_schema import FormatoDocumentoSchema
 
 
-class ValidarDocumentoSchema(Schema):
+class ValidarDocumentoSchema(FormatoDocumentoSchema):
     tipo_documento = fields.Str(required=True)
     num_doc_persona = fields.Str(required=True)
 
 
-class ValidarDocumentoEmailSchema(Schema):
+class ValidarDocumentoEmailSchema(FormatoDocumentoSchema):
     tipo_documento = fields.Str(required=True)
     num_doc_persona = fields.Str(required=True)
     email_confirmado = fields.Email(required=True)
@@ -17,7 +18,7 @@ class ValidarOtpSchema(Schema):
     otp_token = fields.Str(required=True)
     codigo = fields.Str(required=True, validate=validate.Length(min=6, max=6))
 
-class VerificarIdentidadSchema(Schema):
+class VerificarIdentidadSchema(FormatoDocumentoSchema):
     nombre_persona = fields.Str(required=True, validate=validate.Length(min=2, max=50))
     apellido_persona = fields.Str(required=True, validate=validate.Length(min=2, max=50))
     fecha_nacimiento_persona = fields.Date(required=True, format="%Y-%m-%d")
